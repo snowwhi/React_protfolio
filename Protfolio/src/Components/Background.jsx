0d500cfc05467f8cc9e5a927885ae7f0bfa7f9a8
snowwhi <khadijahanif890@gmail.com>
2026-01-21 07:43:11 -0500

    80%

--- Protfolio/src/Components/Background.jsx
+++ Protfolio/src/Components/Background.jsx
@@ -26,7 +26,7 @@ function Background() {
   return (
    <>
    <div  onMouseMove={(e)=>(mouseMoving(e))} className='h-screen w-full bg-white px-2 py-1.5 box-border'> 
-    <div  className=' perspective-[1000px]  p-8 h-screen w-full bg-black rounded-3xl bg-right bg-no-repeat'style={{backgroundImage:`url(${bgimage})`}}>
+    <div  className=' perspective-[1000px] overflow-hidden p-8 h-screen w-full bg-black rounded-3xl bg-right bg-no-repeat'style={{backgroundImage:`url(${bgimage})`}}>
        <img src={logo} className='h-20 w-20 rounded-full ml-10 pt-5'/>
        <div className='mt-25  w-fit perspective-[1000px] ' ref={TiltText}>
      <h1 className='text-[6vw] text-white uppercase font-extrabold leading-[3vw]'>I am <span className='text-7xl text-gray-400 uppercase font-bold'>Baddie</span>™</h1>
--- Protfolio/src/Components/Screen2.jsx
+++ Protfolio/src/Components/Screen2.jsx
@@ -1,32 +1,47 @@
-
+import gsap from 'gsap'
 import { useGSAP } from '@gsap/react'
+import { ScrollTrigger } from 'gsap/all' 
 import '../index.css'
+  gsap.registerPlugin(ScrollTrigger);
 function Screen2() {
-    useGSAP
+
+   useGSAP(function (){
+    gsap.from('.RotateText',{
+      transform:'rotateX(-90deg)',
+      opacity:0,
+      duration:2,
+      stagger:1,
+      scrollTrigger:{
+        trigger:'.RotateText',
+        start:'top 60%',
+        end:'top -100%',
+        scrub:2,
+   
+   }})
+   })
   return (
    <>
-   <div className='bg-white text-black text-center p-10'>
+   <div className='bg-white text-black text-center p-5'>
      <h1 className='mt-5 w-full p-1 flex justify-center text-xl font-bold '>© Developed By Khadija | All Copyright reserved</h1>
    </div>
-   <div>
-    <h1  id = 'RotateText'className='text-[15vw] font-extrabold tracking-tight text-center leading-[15vw]'>IMPACTFULL</h1>
+   <div className='perspective-[1000px]'>
+    <h1  className='RotateText  origin-[33%_75%] text-[13vw] font-extrabold tracking-tight text-center leading-[15vw]'>IMPACTFULL</h1>
    </div>
-      <div>
-    <h1 className='text-[15vw] font-extrabold tracking-tight text-center leading-[15vw] '>Development</h1>
+      <div className='perspective-[1000px]'>
+    <h1 className='RotateText origin-[33%_75%]  text-[13vw] font-extrabold tracking-tight text-center leading-[15vw] '>Development</h1>
    </div>
- <div>
-    <h1 className='text-[15vw] font-extrabold tracking-tight text-center leading-[15vw]'>IS THE</h1>
+ <div className='perspective-[1000px]'>
+    <h1 className='RotateText origin-[33%_75%]  text-[13vw] font-extrabold tracking-tight text-center leading-[15vw]'>IS THE</h1>
    </div>
-    <div>
-    <h1 className='text-[15vw] font-extrabold tracking-tight text-center leading-[15vw]'>ONE</h1>
+    <div className='perspective-[1000px]'>
+    <h1 className='RotateText origin-[33%_75%] text-[13vw] font-extrabold tracking-tight text-center leading-[15vw]'>ONE</h1>
    </div>
-    <div>
-    <h1 className='text-[15vw] font-extrabold tracking-tight text-center leading-[15vw]'>THAT</h1>
+    <div className='perspective-[1000px]'>
+    <h1 className='RotateText origin-[33%_75%] text-[13vw] font-extrabold tracking-tight text-center leading-[15vw]'>THAT</h1>
    </div>
-    <div>
-    <h1 className='text-[15vw] font-extrabold tracking-tight text-center leading-[15vw]'  >WORKS</h1>
+    <div className='perspective-[1000px]'>
+    <h1 className='RotateText origin-[33%_75%] text-[13vw] font-extrabold tracking-tight text-center leading-[15vw]'>WORKS</h1>
    </div>
-
    </>
   )
 }
